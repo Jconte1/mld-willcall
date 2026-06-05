8a6456f8592a0c51cd27ce6468b7125eba615f81
James Conte <James@mld.com>
2026-06-05 13:14:09 -0600

    rederecting all of will call prject to mld.com/willcall

--- next.config.mjs
+++ next.config.mjs
@@ -4,7 +4,26 @@ const basePath = rawBasePath && rawBasePath !== "/" ? rawBasePath.replace(/\/+$/
 
 const nextConfig = {
   reactStrictMode: true,
+
   ...(basePath ? { basePath } : {}),
+
+  async redirects() {
+    return [
+      {
+        source: "/",
+        destination: "https://mld.com/willcall",
+        permanent: true,
+        basePath: false,
+      },
+      {
+        source: "/:path*",
+        destination: "https://mld.com/willcall/:path*",
+        permanent: true,
+        basePath: false,
+      },
+    ];
+  },
+
   webpack(config) {
     // Allow `import url from './file.svg'` to behave like Vite.
     config.module.rules.push({
