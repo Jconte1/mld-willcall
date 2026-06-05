9a96c7b0ec54cb6f604fd0f1edbc93d9fb179560
James Conte <James@mld.com>
2026-06-05 14:01:05 -0600

    added asset prefix

--- next.config.mjs
+++ next.config.mjs
@@ -3,10 +3,17 @@ const rawBasePath = process.env.NEXT_PUBLIC_APP_BASE_PATH || "";
 const basePath =
   rawBasePath && rawBasePath !== "/" ? rawBasePath.replace(/\/+$/, "") : "";
 
+const rawAssetPrefix = process.env.NEXT_PUBLIC_APP_ASSET_PREFIX || "";
+const assetPrefix =
+  rawAssetPrefix && rawAssetPrefix !== "/"
+    ? rawAssetPrefix.replace(/\/+$/, "")
+    : "";
+
 const nextConfig = {
   reactStrictMode: true,
 
   ...(basePath ? { basePath } : {}),
+  ...(assetPrefix ? { assetPrefix } : {}),
 
   webpack(config) {
     config.module.rules.push({
